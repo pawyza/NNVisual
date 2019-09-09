394e40c008cfef5d0d4712e92d381ed0e5132344
pawyza <pawyza.f@gmail.com>
2019-09-10 00:46:41 +0200

    Initialization form added

--- src/menuPackage/ImageNNInitializerController.java
+++ src/menuPackage/ImageNNInitializerController.java
@@ -2,6 +2,7 @@ package menuPackage;
 
 import imageNNPackage.ImageNNController;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -9,13 +10,17 @@ import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
+import javafx.util.converter.DoubleStringConverter;
 import kotlin.text.Regex;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -27,45 +32,133 @@ public class ImageNNInitializerController implements Initializable {
         this.stageReference = stageReference;
     }
 
+    private String heightLastValue = "1";
+    private String widthLastValue = "1";
+    private String learningRateLastValue = "0.000";
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        imageHeight_spin.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1,Integer.MAX_VALUE));
+        imageWidth_spin.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1,Integer.MAX_VALUE));
+
+        SpinnerValueFactory spvDouble = new SpinnerValueFactory.DoubleSpinnerValueFactory(0.0,1.0,0.0,0.001);
+        spvDouble.setConverter(new DoubleStringConverter());
+        learningRate_spin.setValueFactory(spvDouble);
+
+        EventHandler<KeyEvent> keyEventHandler_LR = new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                if(new Regex("[^0-9.]").matches(keyEvent.getText()))
+                    learningRate_spin.getEditor().textProperty().set(learningRateLastValue);
+                learningRateLastValue = learningRate_spin.getEditor().textProperty().get();
+            }
+        };
+
+        learningRate_spin.getEditor().addEventHandler(KeyEvent.KEY_RELEASED,keyEventHandler_LR);
+
+        EventHandler<KeyEvent> keyEventHandler_IH = new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                if(new Regex("/D").matches(keyEvent.getText()))
+                    imageHeight_spin.getEditor().textProperty().set(heightLastValue);
+                try {
+                    if(Integer.parseInt(imageHeight_spin.getEditor().textProperty().get()) < 1) throw new NumberFormatException();;
+                    heightLastValue = imageHeight_spin.getEditor().textProperty().get();
+                } catch (NumberFormatException e){
+                    imageHeight_spin.getEditor().textProperty().set(heightLastValue);
+                }
+            }
+        };
+
+        imageHeight_spin.getEditor().addEventHandler(KeyEvent.KEY_RELEASED,keyEventHandler_IH);
+
+        EventHandler<KeyEvent> keyEventHandler_IW = new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                if(new Regex("/D").matches(keyEvent.getText()))
+                    imageWidth_spin.getEditor().textProperty().set(widthLastValue);
+                try {
+                    if(Integer.parseInt(imageWidth_spin.getEditor().textProperty().get()) < 1) throw new NumberFormatException();
+                    widthLastValue = imageWidth_spin.getEditor().textProperty().get();
+                } catch (NumberFormatException e){
+                    imageWidth_spin.getEditor().textProperty().set(widthLastValue);
+                }
+            }
+        };
+
+        imageWidth_spin.getEditor().addEventHandler(KeyEvent.KEY_RELEASED,keyEventHandler_IW);
+        
+        learningRate_spin.valueProperty().addListener((observableValue, oldValue, newValue) -> {
+            if(!new Regex("\\d.\\d+").matches(newValue.toString())){
+                learningRate_spin.getValueFactory().setValue(oldValue);
+            }
+            checkSetup();
+        });
+
+        inputLayer_txtField.setText("1");
+        outputLayer_txtField.setText("0");
+
         imageHeight_spin.valueProperty().addListener((observableValue, oldValue, newValue) -> {
-            if(newValue <= 0){
-                imageHeight_spin.cancelEdit();
-            }
             if(imageWidth_spin.getValue() > 0){
-                inputLayer_txtField.setText(String.valueOf(imageHeight_spin.getValue()*imageHeight_spin.getValue()));
+                inputLayer_txtField.setText(String.valueOf(imageHeight_spin.getValue()*imageWidth_spin.getValue()));
             }
+            checkSetup();
         });
 
         imageWidth_spin.valueProperty().addListener((obs, oldValue, newValue) ->{
-            if(newValue <= 0){
-                imageWidth_spin.cancelEdit();
-            }
             if(imageHeight_spin.getValue() > 0){
-                inputLayer_txtField.setText(String.valueOf(imageHeight_spin.getValue()*imageHeight_spin.getValue()));
+                inputLayer_txtField.setText(String.valueOf(imageHeight_spin.getValue()*imageWidth_spin.getValue()));
             }
-        });
-
-        hiddenLayersNumber_spin.valueProperty().addListener((obs, oldValue, newValue) ->{
-            if(newValue <= 0){
-                hiddenLayersNumber_spin.cancelEdit();
-            } else {
-                hiddenLayer_txtField.setText("0,".repeat(newValue).substring(0,2*newValue-1));
-            }
+            checkSetup();
         });
 
         hiddenLayer_txtField.textProperty().addListener((obs, oldValue, newValue) ->{{
-            if(!new Regex("[^0-9,]+").containsMatchIn(newValue)){
-                outputLayer_txtField.cancelEdit();
+            if(new Regex("[^0-9,]").containsMatchIn(newValue)){
+                hiddenLayer_txtField.setText(oldValue);
             }
+            checkSetup();
         }});
 
         outputLayer_txtField.textProperty().addListener((obs, oldValue, newValue) ->{
-            if(!new Regex("D+").containsMatchIn(newValue)){
-                outputLayer_txtField.cancelEdit();
+            if(new Regex("\\D").containsMatchIn(newValue)){
+                outputLayer_txtField.setText(oldValue);
             }
+            checkSetup();
         });
+    }
+
+    private void checkSetup() {
+        Integer[] layers = getLayers(hiddenLayer_txtField.getText());
+
+        create_btn.setDisable(true);
+
+        if(layers.length < 3)
+            return;
+
+        for (Integer i: layers) {
+            if(i == 0){ return; }
+        }
+
+        if(learningRate_spin.getValueFactory().getValue() == 0.0)
+            return;
+
+        create_btn.setDisable(false);
+    }
+
+    private Integer[] getLayers(String hiddenLayerText){
+        List<Integer> hiddenLayer = new LinkedList<>();
+
+        String input = inputLayer_txtField.getText();
+        String output = outputLayer_txtField.getText();
+
+        if(!input.isEmpty()) hiddenLayer.add(Integer.valueOf(input));
+        for(String layer : hiddenLayerText.split(",")){
+            if(!layer.isEmpty())
+                hiddenLayer.add(Integer.valueOf(layer));
+        }
+        if(!output.isEmpty()) hiddenLayer.add(Integer.valueOf(output));
+
+        return hiddenLayer.toArray(new Integer[hiddenLayer.size()]);
     }
 
     @FXML
@@ -73,9 +166,6 @@ public class ImageNNInitializerController implements Initializable {
 
     @FXML
     private Spinner<Integer> imageWidth_spin;
-
-    @FXML
-    private Spinner<Integer> hiddenLayersNumber_spin;
 
     @FXML
     private TextField inputLayer_txtField;
